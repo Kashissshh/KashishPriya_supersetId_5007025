5b4e0dea1f171beae139a1eb608bfe97eaca10f8
Kashissshh <132804252+Kashissshh@users.noreply.github.com>
2024-08-07 15:44:32 +0530

    Delete Week_2_Spring_Core_and_Maven directory

--- Week_2_Spring_Core_and_Maven/EXERCISE_1_2_3/src/main/java/com/library/aspect/Aspect.java
+++
@@ -1,5 +0,0 @@
-package com.library.aspect;
-
-public @interface Aspect {
-
-}
--- Week_2_Spring_Core_and_Maven/EXERCISE_1_2_3/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-	public void update() {
-		System.out.println("Updating Book....");
-	}
-
-}
